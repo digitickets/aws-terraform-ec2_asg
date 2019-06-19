85837e29f8e2907ecf2b65c34e0895ff8f43febf
Warrick Hill <warrick.hill@digitickets.co.uk>
2019-06-19 14:11:47 +0100

    output scaling policy arns

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "asg_image_id" {
   description = "Image ID used for EC2 provisioning"
   value       = "${var.image_id != "" ? var.image_id : data.aws_ami.asg_ami.image_id}"
 }
+
+output "asg_scale_up_policy_arns" {
+  description = "List of scale up policy arns to allow external couldwatch alarms to trigger scale up"
+  value = ["${aws_autoscaling_policy.ec2_scale_up_policy.*.arn}"]
+}
+
+output "asg_scale_down_policy_arns" {
+  description = "List of scale down policy arns to allow external couldwatch alarms to trigger scale down"
+  value = ["${aws_autoscaling_policy.ec2_scale_down_policy.*.arn}"]
+}
